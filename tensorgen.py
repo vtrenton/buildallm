35cb774c5ea547929b382b564d810b37888bcf9e
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-12-18 21:19:46 -0600

    added tensor embedding tables and created input_embedding tensor
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- tensorgen.py
+++ tensorgen.py
@@ -40,7 +40,20 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128,
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
 data_iter = iter(dataloader)
-first_batch = next(data_iter)
-print(first_batch)
+
+inputs, targets = next(data_iter)
+print("Inputs:\n", inputs)
+print("Targets:\n", targets)
+
+vocab_size = 20257 # gpt vocab size
+embedding_dim = 3
+
+# generate random weights tensor
+embedding_layer = torch.nn.Embedding(vocab_size, embedding_dim)
+
+# generate input embedding from the input tensor
+input_embedding = embedding_layer(inputs)
+
+print("Input Embedding Tensor:\n", input_embedding)
